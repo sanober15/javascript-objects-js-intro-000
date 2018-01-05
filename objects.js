42c166fa7111cf1c901a8e5ef0e0cb85c06fc8af
sanober riaz <sanober@sanober.me>
2018-01-05 06:00:56 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -6,3 +6,8 @@ playlist = { artist : 'song' };
 function updatePlaylist(playlist, artistName, songTitle){
   return Object.assign(playlist, {[artistName]:'songTitle'});
 }
+
+function removeFromPlaylist(playlist, artistName){
+  delete playlist.artistName;
+  return playlist;
+}
