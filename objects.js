b7f89eb087b342fb4c46caed69d17027137c5e91
sanober riaz <sanober@sanober.me>
2018-01-05 05:59:34 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -1,3 +1,8 @@
 var playlist = {};
 
 playlist = { artist : 'song' };
+
+
+function updatePlaylist(playlist, artistName, songTitle){
+  return Object.assign(playlist, {[artistName]:'songTitle'});
+}
